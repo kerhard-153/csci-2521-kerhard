c9ab7707bfaafa817f2ceef021f94713fa4c83f4
kerhard-153 <kerhard@student.cscc.edu>
2026-02-22 10:55:17 -0500

    Lab 5 - added print function and tested functionality

--- CSCI-2521/Lab5/Lab5_kerhard.cpp
+++ CSCI-2521/Lab5/Lab5_kerhard.cpp
@@ -7,6 +7,11 @@
 
 #include <iostream>
 
+void printInputValidationError()
+{
+	std::cout << "Error: The max digit must be greater than 4 and less than 10. Please try again." << std::endl;
+}
+
 bool isMaxDigitInputValid(int input)
 {
 	return (input > 4 && input < 10);
@@ -29,14 +34,27 @@ int getMaxDigitInput() {
 			return maxDigit;
 		}
 		else {
-			//print error
+			printInputValidationError();
 		}
+	}
+}
+
+void printMultiplicationTable(int maxDigit) {
+
+	for (int i = 1; i <= maxDigit; i++)
+	{
+		for (int j = 1; j <= maxDigit; j++)
+		{
+			std::cout << i * j << "\t";
+		}
+		std::cout << std::endl;
 	}
 }
 
 int main() {
 
 	int maxDigit = getMaxDigitInput();
+	printMultiplicationTable(maxDigit);
 
 	return 0;
 }
