3b286ced41a7930d3b0a9bb5c43a3e4c4c396941
kerhard-153 <kerhard@student.cscc.edu>
2026-03-28 11:52:50 -0400

    Lab 8 setup

---
+++ CSCI-2521/Lab8/Lab8_kerhard.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file Lab8_kerhard.cpp
+ * @author Kate Erhard
+ * @date 2026-03-28
+ * @brief Demonstrates stack vs heap memory and the use of pointers and references.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
